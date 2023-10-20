7cc7774fcf87af66c971fa8e9d518d77e48b0780
s torohi49 <storohi49@gmail.com>
2023-10-20 22:45:01 +0900

    batファイル更新

--- test_of_sinatra/mainapp.bat
+++ test_of_sinatra/mainapp.bat
@@ -6,5 +6,5 @@ set CURDIR=%~dp0
 echo %CURDIR%
 
 REM call bundle exec rerun --no-notify ruby mainapp.rb
-call bundle exec rerun --restart ruby mainapp.rb
+call bundle exec rerun --restart --no-notify --signal ruby mainapp.rb
 
